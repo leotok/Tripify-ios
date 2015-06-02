16dd4972796d01bbbbc664d8fdc5f00f017a91ad
torives <rotcivyves@gmail.com>
2015-06-02 09:46:19 -0300

    LocalDAO adicionado. MoGenarator scrip adicionado a pasta do projeto. Falta getNumberOfRestaurants e getNumberOfNewFriends

---
+++ triploka/triploka/LocalDAO.swift
@@ -0,0 +1,9 @@
+//
+//  LocalDAO.swift
+//  triploka
+//
+//  Created by Victor Yves Crispim on 06/5/15.
+//  Copyright (c) 2015 Leonardo Edelman Wajnsztok. All rights reserved.
+//
+
+import Foundation
