6a584c40cdba4d3755ae8bf1b9a040d955a6a5bc
torives <rotcivyves@gmail.com>
2015-06-12 17:55:47 -0300

    alterei a política de saves do CoreData

--- triploka/triploka/Photo.swift
+++ triploka/triploka/Photo.swift
@@ -55,6 +55,6 @@ class Photo: NSManagedObject {
     func setNewImage(newImage: UIImage){
         
         self.image = newImage
-        LocalDAO.sharedInstance.saveContext()
+        //LocalDAO.sharedInstance.saveContext()
     }
 }
